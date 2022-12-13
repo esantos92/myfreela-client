5c53ad73b5331af44e24e13546860a6d1ea0ff17
Everton Santos <everton.santos@redealumni.com>
2022-12-13 16:37:23 -0300

    :sparkles:feat: header concluded

--- src/Components/MainHeader/index.jsx
+++ src/Components/MainHeader/index.jsx
@@ -1,19 +1,37 @@
-import { Link } from 'react-router-dom'
-import logoMyFreela from "../../assets/logo.png";
+import { Link } from 'react-router-dom';
 
-import './styles.css'
-import '../../assets/global.css'
+import logoMyFreela from "../../assets/logo.png";
+import EditProfileLogo from "../../assets/edit.png"
+import LogoutIcon from "../../assets/log-out.png"
+import './styles.css';
+import '../../assets/global.css';
 
 export const MainHeader = () => {
+  const menuToggle = () => {
+    const toggleMenu = document.querySelector('.menu');
+    toggleMenu.classList.toggle('active');
+  }
+
   return (
     <div className='header'>
-      <button className='logo'>
+      <Link to='/' className='logo'>
         <img src={logoMyFreela} alt="Logo MyFreela" />
-      </button>
+      </Link>
 
-      <button className='avatar'>
-        <img src="https://github.com/esantos92.png" alt="User avatar" />
-      </button>
+      <div className='action'>
+        <button className='avatar' onClick={menuToggle}>
+          <img src="https://github.com/esantos92.png" alt="User avatar" />
+        </button>
+
+        <div className="menu">
+          <h3>Everton Santos</h3>
+          <ul>
+            <li><img src={EditProfileLogo} alt='Edit profile icon'/><span className='title'>Editar Perfil</span></li>
+            <li><img src={LogoutIcon} alt='Logout icon'/><span className='title'>Logout</span></li>
+          </ul>
+        </div>
+
+      </div>
     </div>
   )
 }
